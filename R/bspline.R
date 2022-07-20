11ae59e10174fccd5fc0a0400d4952eebce6aaa7
alexmerot <68989726+alexmerot@users.noreply.github.com>
2022-07-20 07:09:15 +0200

    Improve bspline class validation.

--- R/bspline.R
+++ R/bspline.R
@@ -38,9 +38,9 @@ bspline <- R6Class(
     #' @param K Order of polynomial.
     #' @param m Spline coefficients (\eqn{m = M \cdot D}).
     initialize = function(t_knot, K, m) {
-      stopifnot("t_knot must be numeric." = is.numeric(t_knot))
-      stopifnot("K must be numeric." = is.numeric(K))
-      stopifnot("m must be numeric." = is.numeric(m))
+      stopifnot("t_knot must be numeric." = is.numeric(t_knot) & !is.na(t_knot))
+      stopifnot("K must be numeric." = is.numeric(K) & !is.na(K))
+      stopifnot("m must be numeric." = is.numeric(m) & !is.na(m))
 
       self$K = K
       self$m = m
